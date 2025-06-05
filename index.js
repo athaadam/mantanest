a41cd1c73585a977984e26f86a04c318097d3f3d
athaadam <bontangsuntuk@gmail.com>
2025-06-06 01:25:49 +0700

    add my homepage

---
+++ index.js
@@ -0,0 +1,35 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetID = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetID);
+
+            if (targetElement) {
+            // Hitung posisi scroll target dengan memperhatikan header fixed
+            const headerOffset = 80; // Sesuaikan dengan tinggi header fixed-mu (80px di contoh)
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+            }
+        });
+    });
+
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+
+    document.querySelectorAll('.info-section').forEach(section => {
+      observer.observe(section);
+    });
